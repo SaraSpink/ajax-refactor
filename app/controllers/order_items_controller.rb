e5805b7e6c391cb760e24dd3aea43c15a2192730
Sara Spink <spinkbot@gmail.com>
2017-12-08 16:57:25 -0800

    fix error that was not allowing total items to update in nav bar when you remove an item from the cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -20,11 +20,11 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
+    @order_item = OrderItem.find(params[:id])
     @order = current_order
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    @order_item = OrderItem.find(params[:id])
     respond_to do |format|
       format.html {redirect_to cart_path}
       format.js
